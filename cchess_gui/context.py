ae99a44bf66d4a199fcb2e9449b61d7678c98b2a
zhengran <zhengran@kuaishou.com>
2024-03-31 16:20:24 +0800

    增加思考细节文本框和帮助信息文本框

--- cchess_gui/context.py
+++ cchess_gui/context.py
@@ -5,3 +5,4 @@ class Context:
     def __init__(self):
         self.running = True
         self.board = ChessBoard()
+        self.think_text = "炮二平五 炮8平5 马二进三 马8进7 车一进一 车9平8 车一平六 车8进6 车六进七 马2进1 车九进一 炮2进7"
