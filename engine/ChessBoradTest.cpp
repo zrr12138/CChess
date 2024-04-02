b64b869304b74b87a83f1c8ac386fa9fda3d706f
XiYaoYa0 <2172286013@qq.com>
2024-04-02 19:07:13 +0800

    实现棋谱和棋盘合法性检验

--- engine/ChessBoradTest.cpp
+++ engine/ChessBoradTest.cpp
@@ -13,28 +13,23 @@ using namespace CChess;
 void test1() {
     ChessBoard board;
     board.ClearBoard();
-    board.BoardRed(true);
+    /*board.BoardRed(true);
     board.SetChessAt(Chess(ChessType::Ma, true), 2, 0);
     board.SetChessAt(Chess(ChessType::Xiang, false), 0, 1);
     board.SetChessAt(Chess(ChessType::Bing, true), 2, 1);
     board.SetChessAt(Chess(ChessType::Wang, true), 9, 5);
     board.SetChessAt(Chess(ChessType::Wang, false), 0, 4);
-    board.SetChessAt(Chess(ChessType::Ju, true), 0, 8);
+    board.SetChessAt(Chess(ChessType::Ju, true), 0, 8);*/
+    board.initBoard();
     std::vector<ChessMove> moves;
     std::vector<Chess> dead;
-    board.PrintOnTerminal();
-    //board.GetMoves(&moves);
-    board.GetMovesFrom(2, 0, &moves);
-    board.PrintMoves(&moves);
-    //board.Move(ChessMove(2,0,0,1),&dead);
-    //board.PrintOnTerminal();
-    //board.Move(ChessMove(0, 8, 0, 4),&dead);
-    //board.PrintOnTerminal();
-    /*board.Move(ChessMove(0, 4, 4, 4),&dead);
-    board.PrintOnTerminal();*/
+    std::vector<std::string> QiPu;
+    /*board.GetMovesFrom(2, 0, &moves);
     assert(moves.size() == 2);
     assert(std::find(moves.begin(), moves.end(), ChessMove(2, 0, 0, 1)) != moves.end());
     assert(std::find(moves.begin(), moves.end(), ChessMove(2, 0, 4, 1)) != moves.end());
+    board.Move(ChessMove(9, 5, 8 , 5),&dead);*/
+    board.ChessConversion(ChessMove(7, 1, 7 , 4), &QiPu);
 }
 
 void test2() {
@@ -44,12 +39,11 @@ void test2() {
     board.SetChessAt(Chess(ChessType::Ju, false), 1, 6);
     board.SetChessAt(Chess(ChessType::Pao, true), 1, 8);
     board.SetChessAt(Chess(ChessType::Ju, false), 1, 4);
-
     std::vector<ChessMove> moves;
     std::vector<Chess> dead;
-    board.PrintOnTerminal();
     board.GetMovesFrom(1,8,&moves);
-    board.PrintMoves(&moves);
+    assert(moves.size() ==  11);
+    assert(std::find(moves.begin(), moves.end(), ChessMove(1, 8 ,1, 4)) != moves.end());
 }
 
 void test3() {
@@ -60,9 +54,9 @@ void test3() {
     //board.SetChessAt(Chess(ChessType::Ju, true), 8, 4);
     std::vector<ChessMove> moves;
     std::vector<Chess> dead;
-    board.PrintOnTerminal();
     board.GetMovesFrom(9,5,&moves);
-    board.PrintMoves(&moves);
+    assert(moves.size() == 1);
+    assert(std::find(moves.begin(), moves.end(), ChessMove(9, 5 ,8, 4)) != moves.end());
 }
 
 void test4() {
@@ -73,9 +67,12 @@ void test4() {
     board.SetChessAt(Chess(ChessType::Bing, true), 6,0);
     std::vector<ChessMove> moves;
     std::vector<Chess> dead;
-    board.PrintOnTerminal();
     board.GetMovesFrom(6,0,&moves);
-    board.PrintMoves(&moves);
+    assert(moves.size() == 2);
+    assert(std::find(moves.begin(), moves.end(), ChessMove(6, 0 ,6, 1)) != moves.end());
+    assert(std::find(moves.begin(), moves.end(), ChessMove(6, 0 ,7, 0)) != moves.end());
+    board.GetMovesFrom(1,4,&moves);
+    assert(moves.size() == 3);
 }
 
 void test5() {
@@ -88,37 +85,42 @@ void test5() {
     board.SetChessAt(Chess(ChessType::Bing, true), 8,3);
     std::vector<ChessMove> moves;
     std::vector<Chess> dead;
-    board.PrintOnTerminal();
     board.GetMovesFrom(7,4,&moves);
-    board.PrintMoves(&moves);
+    assert(moves.size() == 3);
+    assert(std::find(moves.begin(), moves.end(), ChessMove(7, 4 ,9, 6)) != moves.end());
+    assert(std::find(moves.begin(), moves.end(), ChessMove(7, 4 ,5, 6)) != moves.end());
 }
 
 void test6() {
     ChessBoard board;
     board.initBoard();
-    board.PrintOnTerminal();
     std::vector<ChessMove> moves;
     std::vector<Chess> dead;
     board.GetMovesFrom(7, 1, &moves);
-    board.PrintMoves(&moves);
     board.Move(ChessMove(7, 1, 0, 1),&dead);
-    board.PrintOnTerminal();
-    board.PrintDead(&dead);
+    assert(dead.size() == 1);
+
 }
 
 void test7() {
-    int a;
-    int b;
-    a = 1;
-    b = 2;
-    int c = 4;
-    assert(a + b == c);
+    ChessBoard board;
+    board.ClearBoard();
+    board.BoardRed(false);
+    board.SetChessAt(Chess(ChessType::Wang, false), 7, 4);
+    board.SetChessAt(Chess(ChessType::Shi, true), 7,5);
+    board.SetChessAt(Chess(ChessType::Bing, false), 1, 1);
+    board.SetChessAt(Chess(ChessType::Shi, true), 1,4);
+    board.SetChessAt(Chess(ChessType::Xiang, true), 4,2);
+    board.SetChessAt(Chess(ChessType::Ma, true), 2,3);
+    //board.SetChessAt(Chess(ChessType::Ma, true), 6,3);
+    assert(board.Legal() == "Legal");
 }
 int main(int argc, char *argv[]) {
     test1();
     //test2();
     //test3();
     //test4();
+    //test5();
     //test6();
     //test7();
 }
